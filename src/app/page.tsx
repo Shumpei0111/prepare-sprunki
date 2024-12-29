c505233db28f37ff7a34baac32c7577d804444bc
Shumpei <shumpei.o.7g@gmail.com>
2024-12-29 12:11:12 +0900

    feat(package): install shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,20 @@ import Clock from "@/components/clock/clock";
 import { Contents } from "@/components/contents/contents";
 import { SprunkiList } from "@/components/sprunki-list/sprunki-list";
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 export default function Home() {
   return (
     <article className="p-4 relative min-h-screen">
       <div className="relative">
+        <Popover>
+          <PopoverTrigger>Open</PopoverTrigger>
+          <PopoverContent>Place content for the popover here.</PopoverContent>
+        </Popover>
         <div className="absolute right-0">
           <Clock />
         </div>
